753b2f4d76d9529742ce124dc004f3309cdfa458
Louai99k <39621424+Louai99k@users.noreply.github.com>
2023-11-25 22:45:16 +0300

    add routes

--- app/(routes)/api/master-info/route.ts
+++ app/(routes)/api/master-info/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import orm from "@src/services/orm";
 
 export async function GET() {
-  const sql = "SELECT * FROM players WHERE is_master=1";
+  const sql = "SELECT * FROM players WHERE is_master = ?";
 
-  const data = await orm(sql, []);
+  const data = await orm(sql, [1]);
 
   return NextResponse.json(data[0]);
 }
--- app/(routes)/api/route.ts
+++ app/(routes)/api/route.ts
@@ -1,5 +1,11 @@
-import { NextResponse } from "next/server";
+import fs from "fs";
 
 export async function GET() {
-  return NextResponse.json({ api: true });
+  const blob = fs.readFileSync("./database/data.db");
+  const res = new Response(blob);
+  const date = new Date().toISOString().replace(":", "-");
+
+  res.headers.set("Content-Disposition", `attachment; filename=${date}.db`);
+
+  return res;
 }
