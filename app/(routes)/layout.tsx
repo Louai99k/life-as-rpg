60148655cef217b4bf363860d3ef022c6016ed4a
Louai99k <39621424+Louai99k@users.noreply.github.com>
2023-11-25 11:53:23 +0300

    change file struct

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css";
 
 interface RootLayoutProps extends React.PropsWithChildren {}
 
+export const metadata = {
+  title: "Life As RPG",
+};
+
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
