3bf2f9fd6ecac23263dc04f9369960d69b279a8e
Louai99k <39621424+Louai99k@users.noreply.github.com>
2023-11-25 23:57:07 +0300

    remove fetching dynamic data from server

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,9 +1,5 @@
 import MainLayout from "@src/components/Main/MainLayout";
-import { notFound } from "next/navigation";
 import "./globals.css";
-import urlGenerator from "@src/utils/urlGenerator";
-
-import type { Player } from "@src/types/player";
 
 interface RootLayoutProps extends React.PropsWithChildren {}
 
@@ -11,29 +7,11 @@ export const metadata = {
   title: "Life As RPG",
 };
 
-const getMasterStats = async (): Promise<Player | null> => {
-  const res = await fetch(urlGenerator("master-info"), {
-    cache: "no-cache",
-  });
-
-  if (!res.ok) {
-    return null;
-  }
-
-  return res.json();
-};
-
 const RootLayout = async ({ children }: RootLayoutProps) => {
-  const data = await getMasterStats();
-
-  if (!data) {
-    notFound();
-  }
-
   return (
     <html lang="en">
       <body>
-        <MainLayout player={data}>{children}</MainLayout>
+        <MainLayout>{children}</MainLayout>
       </body>
     </html>
   );
