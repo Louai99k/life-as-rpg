b91a0643767520b15ebafafa9e2aa565bd6833fb
Louai99k <39621424+Louai99k@users.noreply.github.com>
2023-12-12 00:49:02 +0300

    add comment

--- app/src/utils/game/calculateMagicPrice.ts
+++ app/src/utils/game/calculateMagicPrice.ts
@@ -1,7 +1,7 @@
 import type { Player } from "@src/types/player";
 
 const calculateMagicPrice = (player: Player) => {
-  return Math.min(Math.pow(2, player.magic.length - 1) * 15000, 10000000);
+  return Math.min(Math.pow(2, player.magic.length - 1) * 15000, 10000000); // the -1 is because the player initially has the time category
 };
 
 export default calculateMagicPrice;
