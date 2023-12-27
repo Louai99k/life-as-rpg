611ab2235ec817b539bc3fdcfa643a4de1630377
Louai99k <39621424+Louai99k@users.noreply.github.com>
2023-12-27 20:33:39 +0300

    add pwa

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -3,8 +3,44 @@ import "./globals.css";
 
 interface RootLayoutProps extends React.PropsWithChildren {}
 
+const APP_NAME = "Life As RPG";
+const APP_DEFAULT_TITLE = "Life As RPG App";
+const APP_TITLE_TEMPLATE = "%s - LaR";
+const APP_DESCRIPTION = "My LaR";
+
 export const metadata = {
-  title: "Life As RPG",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 const RootLayout = async ({ children }: RootLayoutProps) => {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-/** @type {import('next').NextConfig} */
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async redirects() {
     return [
       {
@@ -18,4 +24,12 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require("@ducanh2912/next-pwa").default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
